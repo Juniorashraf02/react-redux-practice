7cbd2aef79ac46573539148120b0ba8ca44e5b76
juniorashraf02 <shuvo.istiak11@gmail.com>
2022-12-25 02:40:19 +0600

    boiler plate copied

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import { RouterProvider } from "react-router-dom";
-import ProductProvider from "./context/ProductProvider";
 import routes from "./routes/routes";
 
 function App() {
   return (
     <div>
-      <ProductProvider>
+      {/* <ProductProvider> */}
         <RouterProvider router={routes} />
-      </ProductProvider>
+      {/* </ProductProvider> */}
     </div>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,37 +1,7 @@
-import React from "react";
-import ProductCard from "../components/ProductCard";
-import { useProducts } from "../context/ProductProvider";
+import React from 'react'
 
-const Home = () => {
-  const {
-    state: { products, loading, error },
-  } = useProducts();
-
-  let content;
-
-  if (loading) {
-    content = <p>Loading</p>;
-  }
-
-  if (error) {
-    content = <p>Something went wrong</p>;
-  }
-
-  if (!loading && !error && products.length === 0) {
-    content = <p>Nothing to show, product list is empty</p>;
-  }
-
-  if (!loading && !error && products.length) {
-    content = products.map((product) => (
-      <ProductCard key={product._id} product={product} />
-    ));
-  }
-
+export default function Home() {
   return (
-    <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-7xl gap-14 mx-auto my-10'>
-      {content}
-    </div>
-  );
-};
-
-export default Home;
+    <div>Home</div>
+  )
+}
